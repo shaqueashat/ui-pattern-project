c5f0b87255638765b57b0dd0c23eab8e5d99540f
Shaqueasha Thompkins <shaqueasha_thompkins@yahoo.com>
2020-03-22 23:28:04 -0400

    Updating 2 fetch methods to UI project

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 
-const url = 'https://sandbox-api.brewerydb.com/v2/'
+const url = "https://rickandmortyapi.com/api/"
 
-let randomBeer = document.querySelector(".randomBeerImage")
+let randomBeerImg = document.querySelector(".randomBeerImage")
 
-let randomBeerButton = document.querySelector(".randomBeerButton")
+let prevBeerButton = document.querySelector(".prevBeerButton")
 
-randomButton.addEventListener("click", function (e) {
+let nextBeerButton = document.querySelector(".nextBeerButton")
+
+// randomBeerImg.addEventListener("click", function (e) {
+//     e.preventDefault()
+//   randomButton.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     fetch(url)
+//         .then(res => {
+//             return res.json();
+//         })
+//         .then(res => {
+//             console.log(res)
+//             // console.log("success!", res[0].url);
+//             randomImg.setAttribute("src", res[0].url);
+//         })
+//         .catch(err => {
+//             console.log("something went wrong...", err);
+//         });
+// })
+    // });
+
+    prevBeerButton.addEventListener("click", function (e) {
+        e.preventDefault()
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(res => {
+                console.log(res)
+                // console.log("success!", res[0].url);
+                randomBeerImg.setAttribute("src", res[0].url);
+            })
+            .catch(err => {
+                console.log("something went wrong...", err);
+            });
+    })
+
+    nextBeerButton.addEventListener("click", function (e) {
     e.preventDefault()
     fetch(url)
         .then(res => {
@@ -13,30 +50,32 @@ randomButton.addEventListener("click", function (e) {
         })
         .then(res => {
             console.log(res)
-            randomBeer.setAttribute("src", res[0].url);
+            // console.log("success!", res[0].url);
+            randomBeerImg.setAttribute("src", res[0].url);
         })
         .catch(err => {
             console.log("something went wrong...", err);
         });
 })
 
-categoryButton.addEventListener("click", function (e) {
-    e.preventDefault()
-    fetch(categories, {
-        headers: {
-            'x-api-key': 'dd4354c477a80cf9537076c1f73b8937'
-        }
-    })
-        .then(res => {
-            return res.json();
-        })
-        .then(res => {
-            categoryImg.setAttribute("src", res[0].url)
-        })
-        .catch(err => {
-            console.log("something went wrong...", err);
-        })
-});
+
+// categoryButton.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     fetch(categories, {
+//         headers: {
+//             'x-api-key': 'dd4354c477a80cf9537076c1f73b8937'
+//         }
+//     })
+//         .then(res => {
+//             return res.json();
+//         })
+//         .then(res => {
+//             categoryImg.setAttribute("src", res[0].url)
+//         })
+//         .catch(err => {
+//             console.log("something went wrong...", err);
+//         })
+// });
 
 
 
@@ -68,5 +107,3 @@ categoryButton.addEventListener("click", function (e) {
 //     }
 
 //     document.getElementById('slide').src = images[i];
-
-        
